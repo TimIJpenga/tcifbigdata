f9b052705e3037286db902254fd6bda1e8bb6b1b
Tim <tim.ijpenga@student.hu.nl>
2016-04-10 14:42:17 +0200

    added letter frequency

--- code/wordcount/src/main/java/nl/hu/hadoop/EnglishWords.java
+++ code/wordcount/src/main/java/nl/hu/hadoop/EnglishWords.java
@@ -9,12 +9,14 @@ import org.apache.hadoop.mapreduce.Reducer;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.stream.Stream;
+import java.util.HashMap;
+import java.util.Map;
 
 public class EnglishWords {
+
+    private static Map<String, Integer> letterPatterns;
+    private static int highest = 0;
+    private static int average = 0;
 
     public static void main(String[] args) throws Exception {
 
@@ -36,31 +38,89 @@ public class EnglishWords {
 //        job.waitForCompletion(true);
 
 //        System.out.println(predict("Pluisje"));
-        readLetterPatternResults();
+        System.out.println(calculateLetterAverage("Pluisje", 0.0));
+        letterPatterns = readLetterPatternResults();
+        System.err.println(ComplexPredict("floepsie"));
+        System.err.println(ComplexPredict("train"));
+        System.err.println(ComplexPredict("work"));
+        System.err.println(ComplexPredict("he"));
     }
 
     private static Double predict(String word) throws IOException {
-        Stream letterPatronen = Files.lines(Paths.get("result/letter_patronen/part-r-00000"), StandardCharsets.UTF_8);
-        System.err.println("DIT ZIJN DE PATRONEN:\n" + letterPatronen.findFirst());
+
 
         return 0.0;
     }
 
-    private static void readLetterPatternResults() {
+    private static Double calculateLetterAverage(String word, Double chance) {
+        Double difference = word.length() - 5.0;
+        if (difference < 0) {
+            difference -= difference;
+            System.out.println(difference);
+        }
+        if (difference != 0) {
+            chance = chance / difference;
+            System.out.println(chance);
+        }
+        return chance;
+    }
+
+
+    public static Double ComplexPredict(String word) {
+
+        Double divisors = 0.0;
+        Double sum = 0.0;
+        for (int i = 0; i < word.length(); i++) {
+
+            try {
+                sum += calculateChanceofLetterSequence(word.charAt(i), word.charAt(i + 1));
+            } catch (StringIndexOutOfBoundsException e) {
+//                throw e;
+            }
+            divisors++;
+        }
+
+        return sum / divisors;
+    }
+
+    public static Double calculateChanceofLetterSequence(char first, char second) {
+
+        Integer events = letterPatterns.get(first + "" + second);
+
+        if (events == null) {
+            return 0.0;
+        }
+        return (double) events / highest * 2;
+
+    }
+
+    private static Map<String, Integer> readLetterPatternResults() {
 
         String fileName = "result/letter_patronen/part-r-00000";
 
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            Map<String, Integer> letterPatterns = new HashMap<>();
 
             String line;
+            int index = 0;
+            int total = 0;
             while ((line = br.readLine()) != null) {
-                System.out.println(line);
+                String[] split = line.split("\\s");
+                String key = split[0] + split[1];
+                Integer value = Integer.parseInt(split[2]);
+
+                letterPatterns.put(key, value);
+                total += value;
+                if(value > highest) highest = value;
+                index++;
             }
+            average = total / index;
+            return letterPatterns;
 
         } catch (IOException e) {
             e.printStackTrace();
+            return null;
         }
-        
     }
 }
 
@@ -93,12 +153,6 @@ class EnglishWordsMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
         }
     }
 
-    private Double predict(String word) throws IOException {
-        Stream letterPatronen = Files.lines(Paths.get("result/letter_patronen/part-r-00000"), StandardCharsets.UTF_8);
-        System.err.println("DIT ZIJN DE PATRONEN:\n" + letterPatronen);
-
-        return 0.0;
-    }
 }
 
 class EnglishWordsReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
